7249fac92a2672c40c5c8e8aa40240c12be1d1bf
julio-DevI <julio.a.valenca@gmail.com>
2024-03-21 15:34:34 -0300

    feat: make migration add_company_id_sales_commission_view

--- database/migrations/2024_03_21_182852_add_company_id_to_sales_commission_view.php
+++ database/migrations/2024_03_21_182852_add_company_id_to_sales_commission_view.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -11,9 +12,29 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('sales_commission_view', function (Blueprint $table) {
-            $table->foreignId("company_id")->constrained();
-        });
+        DB::statement("DROP MATERIALIZED VIEW IF EXISTS sales_commission_view");
+
+        $query = DB::table('sales as s')
+            ->join('sellers as sl', 'sl.id', '=' ,'s.seller_id')
+            ->join('clients as cl', 'cl.id','=','s.client_id')
+            ->join('companies as cp', 'cp.id','=','sl.company_id')
+            ->join('addresses as ad', 'ad.id', '=', 'cl.address_id')
+            ->join('users as us', 'us.id', '=', 'sl.user_id')
+            ->join('users as uc', 'uc.id', '=', 'cl.user_id')
+            ->selectRaw("
+                sl.company_id,
+                cp.name as company,
+                us.name as seller,
+                uc.name as client,
+                ad.city,
+                ad.state,
+                s.sold_at,
+                s.status,
+                s.total_amount,
+                round(s.total_amount * cp.commission_rate / 100) as commission
+            ")->toSql();
+
+        DB::statement("CREATE MATERIALIZED VIEW sales_commission_view AS $query");
     }
 
     /**
@@ -21,8 +42,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('sales_commission_view', function (Blueprint $table) {
-            $table->dropConstrainedForeignId('company_id');
-        });
+        DB::statement("DROP MATERIALIZED VIEW sales_commission_view");
     }
 };
